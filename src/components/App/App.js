9e3f02f0b8f914531e20e7dc7ed9056de2252ffd
brent <brent.e.meyer@gmail.com>
2020-04-15 00:06:25 -0400

    adds a slider

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,13 @@ import styles from './styles.module.css';
 
 import { Header } from '../Header';
 import { Footer } from '../Footer';
+import { Slider } from '../Slider';
 
 export const App = () => (
   <div className={ styles.app }>
     <Header />
     <h1>gradient</h1>
+    <Slider />
     <Footer />
   </div>
 );
