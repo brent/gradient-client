1962169cc67539394b9277df9aad8fb9ed5800c0
brent <brent.e.meyer@gmail.com>
2020-04-26 17:29:20 -0400

    adds GradientGenerator class

---
+++ src/utils/GradientGenerator/GradientGenerator.js
@@ -0,0 +1,92 @@
+class GradientGenerator {
+  generateGradient(startHexColor, endHexColor, steps) {
+    // strips '#' from start of values, if present
+    const start = this._sanitize(startHexColor);
+    const end = this._sanitize(endHexColor);
+
+
+    // converts 3 or 6 char hex values to rgb object
+    // ex: fff -> { r: 255, g: 255, b: 255 }
+    const startRgb = start.length === 6
+      ? this._hexToRgb(start)
+      : this._hexToRgb(this._fullHex(start));
+
+    const endRgb = end.length === 6
+      ? this._hexToRgb(end)
+      : this._hexToRgb(this._fullHex(end));
+
+    // what is the range of the gradient across each rgb value?
+    const rDelta = startRgb.r - endRgb.r;
+    const gDelta = startRgb.g - endRgb.g;
+    const bDelta = startRgb.b - endRgb.b;
+
+    // what are the increments to step through acrorss each rgb value?
+    const rStep = Math.round(rDelta/(steps - 1));
+    const gStep = Math.round(gDelta/(steps - 1));
+    const bStep = Math.round(bDelta/(steps - 1));
+
+    // return array
+    let gradient = [];
+
+    // starting value goes in first, converted back to hex
+    gradient.push(this._rgbToHex(startRgb.r, startRgb.g, startRgb.b));
+
+    // step through and generate each rgb value in gradient,
+    // then add its converted hex to return array.
+    ((r, g, b) => {
+      for (let i = steps - 2; i > 0; i--) {
+        r = r - rStep;
+        g = g - gStep;
+        b = b - bStep;
+        gradient.push(this._rgbToHex(r, g, b));
+      }
+    })(startRgb.r, startRgb.g, startRgb.b);
+
+    // add end hex to reutrn array
+    gradient.push(this._rgbToHex(endRgb.r, endRgb.g, endRgb.b));
+
+    return gradient;
+  }
+
+  _rgbToHex(r, g, b) {
+    const hex = [r, g, b].map(val => {
+      val = val.toString(16);
+      return val.length === 1 ? `0${val}` : val;
+    });
+
+    return `#${hex.join('')}`;
+  }
+
+  _hexToRgb(hex) {
+    if (hex.length !== 6) return null;
+
+    const r = parseInt(hex.slice(0, 2), 16);
+    const g = parseInt(hex.slice(2, 4), 16);
+    const b = parseInt(hex.slice(4), 16);
+
+    return {
+      r: r,
+      g: g,
+      b: b,
+    };
+  }
+
+  _sanitize(str) {
+    return str.charAt(0) !== '#'
+      ? str
+      : str.replace('#', '');
+  }
+
+  _fullHex(shortHex) {
+    if (shortHex.length !== 3) return null;
+
+    let parts = shortHex.split('');
+    parts.forEach((part, i) => {
+      parts[i] = `${part}${part}`;
+    });
+
+    return parts.join('');
+  }
+}
+
+export default new GradientGenerator();
---
+++ src/utils/GradientGenerator/GradientGenerator.test.js
@@ -0,0 +1,89 @@
+import GradientGenerator from './GradientGenerator';
+
+describe('GradientGenerator', () => {
+  test('it returns an array', () => {
+    const startColor = '1a2b3c';
+    const endColor = 'a1b2c3';
+
+    const result = GradientGenerator.generateGradient(startColor, endColor, 8);
+
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  test('array length equals amount of specified steps', () => {
+    const startColor = 'fedcba';
+    const endColor = '012345';
+
+    const result = GradientGenerator.generateGradient(startColor, endColor, 10);
+    const expectedLength = 10;
+
+    expect(result.length).toEqual(expectedLength);
+  });
+
+  test('it returns an array of hex colors', () => {
+    const result = GradientGenerator.generateGradient('eee', 'aaa', 5);
+    const expected = ['#eeeeee', '#dddddd', '#cccccc', '#bbbbbb', '#aaaaaa'];
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('rgbToHex', () => {
+  test('it converts an rgb value to hex', () => {
+    expect(GradientGenerator._rgbToHex(1,1,1)).toEqual('#010101');
+  });
+});
+
+describe('hexToRgb', () => {
+  const testHex = 'a1b2c3';
+
+  test('it only takes full character hex strings', () => {
+    expect(GradientGenerator._hexToRgb('a1b2')).toBeNull();
+  });
+
+  test('returns an object', () => {
+    expect(typeof GradientGenerator._hexToRgb(testHex)).toEqual('object');
+  });
+
+  test('it converts hex to rgb', () => {
+    const expectedResult = { r: 161, g: 178, b: 195};
+    const result = GradientGenerator._hexToRgb(testHex);
+
+    expect(typeof result.r).toBe('number');
+    expect(typeof result.g).toBe('number');
+    expect(typeof result.b).toBe('number');
+
+    expect(result).toEqual(expectedResult);
+  });
+});
+
+describe('sanitize', () => {
+  test('it removes the # sign from a string', () => {
+    const testString = '#eee';
+    const expectedResult = 'eee';
+
+    expect(GradientGenerator._sanitize(testString)).toEqual(expectedResult);
+  });
+
+  test('it only removes # from the beginning of a string', () => {
+    const testString = 'ee#e';
+    const expectedResult = 'ee#e';
+
+    expect(GradientGenerator._sanitize(testString)).toEqual(expectedResult);
+  });
+});
+
+describe('fullHex', () => {
+  test('it only takes 3 character hex strings', () => {
+    const testString = 'eeee';
+
+    expect(GradientGenerator._fullHex(testString)).toBeNull();
+  });
+
+  test('it converts a 3 character hex to 6 character hex', () => {
+    const testHex = 'abc';
+    const expectedResult = 'aabbcc';
+
+    expect(GradientGenerator._fullHex(testHex)).toEqual(expectedResult);
+  });
+});
