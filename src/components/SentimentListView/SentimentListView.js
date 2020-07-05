9ed00acc6c88cf314f53d52ca7022b12ae46f8d3
brent <brent.e.meyer@gmail.com>
2020-07-05 13:30:03 -0400

    adds sentiment color background to list items

--- src/components/SentimentListView/SentimentListView.js
+++ src/components/SentimentListView/SentimentListView.js
@@ -10,14 +10,24 @@ const getEntries = async (api) => {
 };
 
 const renderEntries = (entries) => {
-  return entries.map(entry => (
-    <li key={entry.id}>
-      <p>color: { entry.color }</p>
-      <p>sentiment: { entry.sentiment }</p>
-      <p>created: { entry.created_at }</p>
-    </li>
-  ));
+  return entries.map(entry => <SentimentListItem key={entry.id} entry={entry} />);
 };
+
+const SentimentListItem = ({
+  entry: { id, color, sentiment, created_at }
+}) => {
+  return (
+    <li
+      key={id}
+      className={styles.sentimentListItem}
+      style={{ background: `#${color}` }}
+    >
+      <p>color: {color}</p>
+      <p>sentiment: {sentiment}</p>
+      <p>created: {created_at}</p>
+    </li>
+  );
+}
 
 const SentimentListView = () => {
   const [entries, setEntries] = useState([]);
@@ -34,12 +44,17 @@ const SentimentListView = () => {
   }, []);
 
   return (
-    <AppView className={ styles.sentimentListView }>
-      <h2>SentimentListView</h2>
-      { isLoading
-          ? <p>Loading...</p>
-          : renderEntries(entries)
-      }
+    <AppView
+      type={ appViewType.borderless }
+      className={ styles.sentimentListView }
+    >
+      <section className='main'>
+        <h2>SentimentListView</h2>
+        { isLoading
+            ? <p>Loading...</p>
+            : renderEntries(entries)
+        }
+      </section>
     </AppView>
   );
 }
