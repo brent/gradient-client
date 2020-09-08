a7c1e4f2ce7f3bf2e74997df0dbed0e8174fdba3
brent <brent.e.meyer@gmail.com>
2020-09-07 20:00:05 -0400

    adds styles to entryDetailView

--- src/components/EntryDetailView/EntryDetailView.js
+++ src/components/EntryDetailView/EntryDetailView.js
@@ -1,28 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { AppView, appViewType } from '../AppView';
 import { ContentCard } from '../ContentCard';
+import styles from './styles.module.css';
+import moment from 'moment';
 
 export const EntryDetailView = (props) => {
+  const history = useHistory();
   const entry = props.location.state.entry;
+  const dayOfWeek = moment(entry.created_at).format('dddd');
+  const date = moment(entry.created_at).format('MMM Do, YYYY');
+
   return (
     <AppView
-      className='entryDetailView'
+      className={ styles.entryDetailView }
       style={{ backgroundColor: `#${entry.color}` }}
       type={ appViewType.fullBleed }
     >
-      <p className='entryDate'>{ entry.created_at }</p>
-      <p className='entryColor'>{ entry.color }</p>
-      <ContentCard>
+      <div className={ styles.entryMetadata }>
+        <p className={ styles.entryDate }>{ date }</p>
+        <p className={ styles.entryDay }>{ dayOfWeek }</p>
+        <p className={ styles.entryColor }>#{ entry.color }</p>
+      </div>
+      <ContentCard className={ styles.noteContentWrapper }>
         {
           entry.note_content ? (
             <p
-              className='noteContent'
+              className={ styles.noteContent }
               dangerouslySetInnerHTML={{ __html: entry.note_content }}
             />
           ) : null
         }
         <button
-          className='closeBtn'
+          className={ styles.closeBtn }
           style={{ backgroundColor: `#${entry.color}` }}
         >
           Close
