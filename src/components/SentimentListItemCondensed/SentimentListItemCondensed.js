d67be8e2fafe5789f1db3a772ffc695821bd953e
brent <brent.e.meyer@gmail.com>
2021-03-21 15:18:00 -0400

    adds condensed list view block styles

--- src/components/SentimentListItemCondensed/SentimentListItemCondensed.js
+++ src/components/SentimentListItemCondensed/SentimentListItemCondensed.js
@@ -15,8 +15,16 @@ const SentimentListItemCondensed = ({
       className={ styles.sentimentListItemCondensed }
       onClick={ onClick }
     >
-      <h4 className={ styles.entryDate }>{ entryDate }</h4>
-      <h3 className={ styles.dayOfWeek }>{ dayOfWeek }</h3>
+      <span
+        style={{ background: `#${color}` }}
+        className={ styles.colorBlock }
+      >
+        #{ color }
+      </span>
+      <div className={ styles.dateMetadata }>
+        <h4 className={ styles.entryDate }>{ entryDate }</h4>
+        <h3 className={ styles.dayOfWeek }>{ dayOfWeek }</h3>
+      </div>
     </li>
   );
 }
